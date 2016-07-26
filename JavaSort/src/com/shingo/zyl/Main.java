f139a28c17f2d1dd16a3e5893d8fa2149f980dd4
zylshingo <651809535@qq.com>
2016-07-26 21:50:50 +0800

    111
    111

--- JavaSort/src/com/shingo/zyl/Main.java
+++ JavaSort/src/com/shingo/zyl/Main.java
@@ -23,8 +23,8 @@ public class Main {
                 arr[index] = temp;
             }
         }
-        for (int i = 0;i<arr.length;++i) {
-            System.out.println(arr[i]);
+        for (int anArr : arr) {
+            System.out.println(anArr);
         }
     }
 }
